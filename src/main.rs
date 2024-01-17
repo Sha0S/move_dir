071b4d5310ebc0186acac1ca69fc5c7eb25b56e4
Bernát Süli <suli.bernat@gmail.com>
2024-01-17 07:17:36 +0100

    Add check_folder

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use serde::Deserialize;
-use std::fs::File;
-use std::io::prelude::*;
+#![allow(clippy::upper_case_acronyms)]
 
+use serde::Deserialize;
+use std::fs::{File, self};
+use std::path::{Path, PathBuf};
+use std::io::prelude::*;
+use chrono::*;
 
 #[derive(Debug, Deserialize)]
 struct Config {
@@ -20,23 +23,99 @@ struct Station {
 #[derive(Debug, Deserialize)]
 enum SType { SPI, AOI}
 
-const config_file: &str = ".\\config.toml";
+impl SType {
+    fn as_str(&self) -> String {
+        match self {
+            SType::SPI => "SPI".to_string(),
+            SType::AOI => "AOI".to_string()
+        }
+    }
+}
+
+const CONFIG_FILE: &str = ".\\config.toml";
 fn load_config() -> Config {
-    let mut file = File::open(config_file).expect("Unable to open config file!");
+    let mut file = File::open(CONFIG_FILE).expect("Unable to open config file!");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed to read config file!");
 
     println!("{}", contents);
 
 
-    let config: Config = toml::from_str(contents.as_str()).expect("");
+    let config: Config = toml::from_str(contents.as_str()).expect("Failed to parse TOML config file!");
 
     println!("{:?}", config);
 
     config
 }
 
+// Checking if the folders exist
+fn sanity_check(config: &Config) -> bool {
+    assert!(Path::new(&config.input_dir).exists(), "Input path \"{}\" does NOT exist!", config.input_dir);
+    assert!(Path::new(&config.output_dir).exists(), "Output path \"{}\" does NOT exist!", config.output_dir);
 
-fn main() {
-    load_config();
+    true
+}
+
+fn check_folder(config: &Config, directory: PathBuf) -> Result<(),  std::io::Error> {
+    let time_now = Local::now();
+    let time_limit = Duration::days(config.time_limit as i64);
+
+    for entry in fs::read_dir(directory)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            check_folder(config, path)?;
+        } else {
+            match path.metadata() {
+                Err(err) => panic!("Could not get file metadata! {err}"),
+                Ok(meta) => {
+                    let modification_time: DateTime<Local> = meta.modified().expect("Could not read modification date!").into();
+                    let time_difference = time_now - modification_time;
+
+                    if time_difference > time_limit {
+                        println!("{:?} - {:?}", path, modification_time);
+                        move_file(config, path, modification_time)?;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn move_file(config: &Config, file: PathBuf, time: DateTime<Local>) -> Result<(),  std::io::Error> {
+
+    let station_str = format!("L{}_{}", config.station.line, config.station.name.as_str());
+    let output_folder = format!("{}\\{}\\{}\\{}\\{}\\", config.output_dir, station_str ,time.year(), time.month(), time.day());
+    println!("\t{output_folder}");
+
+    // 1 - Check if destination directory exists, if not, then create it.
+    let mut output_path = PathBuf::from(&output_folder);
+    if !output_path.exists() {
+        fs::create_dir_all(&output_path)?;
+    }
+
+    output_path.set_file_name(file.file_name().expect("Failed to extract file name!"));
+    println!("\t{:?}", output_path);
+
+    // 2 - Calculate checksum
+
+    // 3 - Copy file
+
+    // 4 - Verify checksum
+
+    // 5 - Remove file
+    Ok(())
+}
+
+fn main() -> Result<(),  std::io::Error> {
+    let config = load_config();
+
+    if sanity_check(&config) {
+        let path = PathBuf::from(config.input_dir.clone());
+        check_folder(&config, path)?;
+    }
+
+    Ok(())
 }
